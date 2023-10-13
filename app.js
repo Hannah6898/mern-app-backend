27fd491b32ae28b6fd40931fc2f940ecf1284cfa
Hannah6898 <hannahosibodu68@outlook.com>
2023-10-13 14:53:27 +0100

    Added Port

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yertzeh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
     console.log("Connected to database");
   })
   .catch((err) => {
