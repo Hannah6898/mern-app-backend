19959834251227669140008928b943eb5af8fabb
Hannah6898 <hannahosibodu68@outlook.com>
2023-10-20 15:36:19 +0100

    Refactored database connection

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yertzeh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.csj2iod.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 3000);
